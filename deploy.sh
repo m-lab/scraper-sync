db6ab6b16b6ad23505c525f50b202e87653531ab
Peter Boothe <pboothe@pboothe2.nyc.corp.google.com>
2017-05-24 15:46:10 -0400

    Cleaned up so that the code is less nested and the if branches now mostly only set variables.

--- deploy.sh
+++ deploy.sh
@@ -29,6 +29,34 @@ else
   GIT_COMMIT=$(git log -n 1 | head -n 1 | awk '{print $2}')
 fi
 
+if [[ "$1" == production ]]; then
+  KEY_FILE=/tmp/production-secret-key.json
+  PROJECT=mlab-oti
+  SHEET_ID=143pU25GJidW2KZ_93hgzHdqTqq22wgdxR_3tt3dvrJY
+  NAMESPACE=scraper
+  CLUSTER=scraper-cluster
+  ZONE=us-central1-a
+elif [[ "$1" == staging ]]; then
+  KEY_FILE=/tmp/staging-secret-key.json
+  PROJECT=mlab-staging
+  SHEET_ID=143pU25GJidW2KZ_93hgzHdqTqq22wgdxR_3tt3dvrJY
+  NAMESPACE=scraper
+  CLUSTER=scraper-cluster
+  ZONE=us-central1-a
+elif [[ "$1" == sandbox-* ]]; then
+  SANDBOXSUFFIX=$(echo "$1" | sed -e 's/^sandbox-//')
+  [[ -n "${SANDBOXSUFFIX}" ]] || exit 1
+  KEY_FILE=/tmp/sandbox-secret-key.json
+  PROJECT=mlab-sandbox
+  SHEET_ID=143pU25GJidW2KZ_93hgzHdqTqq22wgdxR_3tt3dvrJY
+  NAMESPACE=scraper-${SANDBOXSUFFIX}
+  CLUSTER=${SANDBOXSUFFIX}-scraper-cluster
+  ZONE=us-central1-a
+else
+  echo "BAD ARGUMENT TO $0"
+  exit 1
+fi
+
 if [[ -e deployment ]]
 then
   echo "existing deployment/ directory is in the way."
@@ -36,49 +64,22 @@ then
 fi
 mkdir deployment
 cp deploy.yml deployment
-if [[ "$1" == production ]]; then
-  if [[ $2 == travis ]]; then
-    gcloud auth activate-service-account --key-file /tmp/production-secret-key.json
-  fi
-  ./travis/substitute_values.sh deployment \
-    IMAGE_URL gcr.io/mlab-oti/github-m-lab-scraper-sync:${GIT_COMMIT} \
-    SPREADSHEET_ID 143pU25GJidW2KZ_93hgzHdqTqq22wgdxR_3tt3dvrJY \
-    NAMESPACE scraper \
-    GITHUB_COMMIT http://github.com/m-lab/scraper-sync/tree/${GIT_COMMIT}
-  ./travis/build_and_push_container.sh \
-    gcr.io/mlab-oti/github-m-lab-scraper-sync:${GIT_COMMIT}  mlab-staging
-  gcloud --project=mlab-oti container clusters get-credentials scraper-cluster \
-    --zone=us-central1-a
-elif [[ "$1" == staging ]]; then
-  if [[ $2 == travis ]]; then
-    gcloud auth activate-service-account --key-file /tmp/staging-secret-key.json
-  fi
-  ./travis/substitute_values.sh deployment \
-    IMAGE_URL gcr.io/mlab-staging/github-m-lab-scraper-sync:${GIT_COMMIT} \
-    SPREADSHEET_ID 143pU25GJidW2KZ_93hgzHdqTqq22wgdxR_3tt3dvrJY \
-    NAMESPACE scraper \
-    GITHUB_COMMIT http://github.com/m-lab/scraper-sync/tree/${GIT_COMMIT}
-  ./travis/build_and_push_container.sh \
-    gcr.io/mlab-staging/github-m-lab-scraper-sync:${GIT_COMMIT}  mlab-staging
-  gcloud --project=mlab-staging container clusters get-credentials scraper-cluster \
-    --zone=us-central1-a
-elif [[ "$1" == sandbox-* ]]; then
-  if [[ $2 == travis ]]; then
-    gcloud auth activate-service-account --key-file /tmp/sandbox-secret-key.json
-  fi
-  SANDBOXSUFFIX=$(echo "$1" | sed -e 's/^sandbox-//')
-  [[ -n "${SANDBOXSUFFIX}" ]] || exit 1
-  ./travis/substitute_values.sh deployment \
-    IMAGE_URL gcr.io/mlab-sandbox/github-m-lab-scraper-sync:${GIT_COMMIT} \
-    SPREADSHEET_ID 143pU25GJidW2KZ_93hgzHdqTqq22wgdxR_3tt3dvrJY \
-    NAMESPACE scraper \
-    GITHUB_COMMIT http://github.com/m-lab/scraper-sync/tree/${GIT_COMMIT}
-  ./travis/build_and_push_container.sh \
-    gcr.io/mlab-sandbox/github-m-lab-scraper-sync:${GIT_COMMIT}  mlab-sandbox
-  gcloud --project=mlab-sandbox container clusters get-credentials ${SANDBOXSUFFIX}-scraper-cluster \
-    --zone=us-central1-a
-else
-  echo "BAD ARGUMENT TO $0"
-  exit 1
+
+if [[ $2 == travis ]]; then
+  gcloud auth activate-service-account --key-file ${KEY_FILE}
 fi
+
+./travis/substitute_values.sh deployment \
+  IMAGE_URL gcr.io/${PROJECT}/github-m-lab-scraper-sync:${GIT_COMMIT} \
+  SPREADSHEET_ID ${SHEET_ID} \
+  NAMESPACE ${NAMESPACE} \
+  GITHUB_COMMIT http://github.com/m-lab/scraper-sync/tree/${GIT_COMMIT}
+
+./travis/build_and_push_container.sh \
+  gcr.io/${PROJECT}/github-m-lab-scraper-sync:${GIT_COMMIT} \
+  ${PROJECT}
+
+gcloud --project=${PROJECT} \
+  container clusters get-credentials ${CLUSTER} --zone=${ZONE}
+
 kubectl apply -f deployment/
