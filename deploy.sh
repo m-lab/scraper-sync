7073168ae1f7df86e72204a0914985cb4c0b4696
Peter Boothe <pboothe@pboothe2.nyc.corp.google.com>
2017-05-24 15:35:02 -0400

    Changed args to explicitly signal travis usage

--- deploy.sh
+++ deploy.sh
@@ -10,12 +10,19 @@
 # locally and you haven't committed your changes, then then URL will be a lie.
 # Don't do those things.
 
+USAGE="$0 [production|staging|arbitrary-string-sandbox] travis?"
+if [[ -n "$2" ]] && [[ "$2" != travis ]]; then
+  echo The second argument can only be the word travis or nothing at all.
+  echo $USAGE
+  exit 1
+fi
+
 set -e
 set -x
 
 source "${HOME}/google-cloud-sdk/path.bash.inc"
 
-if [[ -n "${TRAVIS_BUILD_DIR}" ]]; then
+if [[ $2 == travis ]]; then
   cd $TRAVIS_BUILD_DIR
   GIT_COMMIT=${TRAVIS_COMMIT}
 else
@@ -30,6 +37,9 @@ fi
 mkdir deployment
 cp deploy.yml deployment
 if [[ "$1" == production ]]; then
+  if [[ $2 == travis ]]; then
+    gcloud auth activate-service-account --key-file /tmp/production-secret-key.json
+  fi
   ./travis/substitute_values.sh deployment \
     IMAGE_URL gcr.io/mlab-oti/github-m-lab-scraper-sync:${GIT_COMMIT} \
     SPREADSHEET_ID 143pU25GJidW2KZ_93hgzHdqTqq22wgdxR_3tt3dvrJY \
@@ -40,6 +50,9 @@ if [[ "$1" == production ]]; then
   gcloud --project=mlab-oti container clusters get-credentials scraper-cluster \
     --zone=us-central1-a
 elif [[ "$1" == staging ]]; then
+  if [[ $2 == travis ]]; then
+    gcloud auth activate-service-account --key-file /tmp/staging-secret-key.json
+  fi
   ./travis/substitute_values.sh deployment \
     IMAGE_URL gcr.io/mlab-staging/github-m-lab-scraper-sync:${GIT_COMMIT} \
     SPREADSHEET_ID 143pU25GJidW2KZ_93hgzHdqTqq22wgdxR_3tt3dvrJY \
@@ -50,6 +63,9 @@ elif [[ "$1" == staging ]]; then
   gcloud --project=mlab-staging container clusters get-credentials scraper-cluster \
     --zone=us-central1-a
 elif [[ "$1" == sandbox-* ]]; then
+  if [[ $2 == travis ]]; then
+    gcloud auth activate-service-account --key-file /tmp/sandbox-secret-key.json
+  fi
   SANDBOXSUFFIX=$(echo "$1" | sed -e 's/^sandbox-//')
   [[ -n "${SANDBOXSUFFIX}" ]] || exit 1
   ./travis/substitute_values.sh deployment \
